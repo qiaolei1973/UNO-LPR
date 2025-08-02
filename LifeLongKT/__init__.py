caae4f1b722f77145a98cc614ba4b5ecab0b50c1
PengLinzhi <73518557+PengLinzhi@users.noreply.github.com>
2025-08-02 16:18:49 +0800

    Update __init__.py

--- LifeLongKT/__init__.py
+++ LifeLongKT/__init__.py
@@ -1,3 +1,3 @@
-from .DKT import DKT, POMDPDKT, featureEmbedding, SASRec_KT, GRU4Rec_KT
+from .DKT import DKT, UniLPR, featureEmbedding, SASRec_KT, GRU4Rec_KT
 from .DataLoad import SeqDataset, PaddingCollateFn
 from .utils import related_loss
