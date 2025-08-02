b8141c6def4a7ccdf0680baecca31bd2fb066a9f
PengLinzhi <73518557+PengLinzhi@users.noreply.github.com>
2025-08-02 16:19:56 +0800

    Update UniLPR_PPO.py

--- Agent/UniLPR_PPO.py
+++ Agent/UniLPR_PPO.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from .agent_utils import RL_Data, Memory, PolicyNet, ValueNet
-from LifeLongKT import POMDPDKT, related_loss
+from LifeLongKT import UniLPR, related_loss
 import random
 
 class UniLPR_PPO(nn.Module):
@@ -31,7 +31,7 @@ class UniLPR_PPO(nn.Module):
         self.seq_beta = seq_beta
         self.kt_beta = kt_beta
 
-        self.UniRec_model = POMDPDKT(action_dim, device, hidden_dim, hidden_dim,max_len,sigmoid_func=True)
+        self.UniRec_model = UniLPR(action_dim, device, hidden_dim, hidden_dim,max_len,sigmoid_func=True)
         self.output_fc = nn.Sequential(nn.Linear(hidden_dim, hidden_dim*2), nn.ReLU(), nn.Linear(hidden_dim*2, hidden_dim))
         
         input_dim = hidden_dim
